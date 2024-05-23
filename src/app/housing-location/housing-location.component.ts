2582c7c194a7b7c7df62faaf989b93fe04719248
Ayush2006128 <ayush.muley1907@outlook.com>
2024-05-23 09:46:34 +0530

    finished

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -17,6 +17,7 @@ import {HousingLocation} from '../housinglocation';
       />
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
   styleUrl: './housing-location.component.css'
